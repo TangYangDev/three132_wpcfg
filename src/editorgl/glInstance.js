1931094f7014ba7b10b99a151017650fd476b45e
TangYangDev <610152686@qq.com>
2021-08-30 02:38:18 +0800

    测试封装提交

--- src/editorgl/glInstance.js
+++ src/editorgl/glInstance.js
@@ -2,14 +2,14 @@
  * 定义glInstance初始化类
  */
 
-import boxGeometryWidget from "./widgets/boxGeometryWidget";
-
+import boxGeometryWidget from './widgets/boxGeometryWidget';
 
 export default class glInstance {
     init() {
-        const boxGeo = new boxGeometryWidget();
-        boxGeo.renderer.setClearColor(0x000000);
-        boxGeo.renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(boxGeo.renderer.domElement);
+        const boxBaseObj = new boxGeometryWidget()
+        boxBaseObj.renderer.setClearColor(0x000000);
+        boxBaseObj.renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(boxBaseObj.renderer.domElement);
+        boxBaseObj.render();
     }
 }
--- src/editorgl/widgets/boxGeometryWidget.js
+++ src/editorgl/widgets/boxGeometryWidget.js
@@ -1,21 +1,34 @@
+import THREEBaseObject from "../../base/THREEBaseObject";
 import utilTHREE from "../../utils/utilTHREE";
-export default class boxGeometryWidget {
+
+
+export default class boxGeometryWidget extends THREEBaseObject {
     constructor() {
+        super();
         this.scene = utilTHREE.scene();
         this.renderer = utilTHREE.renderer({ antialias: true });
         this.camera = utilTHREE.perspectiveCamera(70, window.innerWidth / innerHeight, 0.01, 100);
         this.geometry = utilTHREE.geo.box(0.2, 0.2, 0.2);
         this.material = utilTHREE.material.normal();
-        this.mesh = utilTHREE.mesh(this.geometry, this.material);
-        this.scene.add(this.mesh);
+        this.mesh = this.meshAddGeometryAndMaterial();
         this.camera.position.z = 1;
-        this.animate();
     }
 
-    animate() {
-        requestAnimationFrame(this.animate.bind(this));
-        this.mesh.rotation.x += 0.01;
-        this.mesh.rotation.y += 0.02;
-        this.renderer.render(this.scene, this.camera);
+    render() {
+        this.sceneAddMeshes();
+        this.animationFrame();
     }
 }
+
+// export default boxBaseObj = {
+//     scene: utilTHREE.scene(),
+//     renderer: utilTHREE.renderer({ antialias: true }),
+//     camera: utilTHREE.perspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 100),
+//     geometry: utilTHREE.geo.box(0.2, 0.2, 0.2),
+//     material: utilTHREE.material.normal(),
+//     mesh: THREEBaseObject.GeometryAndMaterialAddMesh(),
+
+//     sceneAddMeshes() {
+//         THREEBaseObject.SceneAddMeshes();
+//     }
+// }
